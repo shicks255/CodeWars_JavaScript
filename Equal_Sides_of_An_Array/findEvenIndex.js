5da02e61d6a32e5cebd882aa61407d86fbc1b03e
shicks <shicks255@yahoo.com>
2018-06-08 10:15:27 -0400

    this problem i think is done but i would like to refactor it more

---
+++ Equal_Sides_of_An_Array/findEvenIndex.js
@@ -0,0 +1,30 @@
+function findEvenIndex(array)
+{
+
+    const length = array.length;
+    //find mid point of array, rounded down.
+    //subtract 1 for 0 based to make it easier
+    var middle = Math.floor(array.length / 2) - 1;
+
+    for (let i = middle; i > 0; i--)
+    {
+        const firstHalfSum = array.slice(0, i).reduce( (a, b) => a+b);
+        const secondHalf = array.slice(i+1, length).reduce( (a,b) => a+b);
+
+        if (firstHalfSum === secondHalf)
+            return i;
+    }
+
+    middle = Math.ceil(array.length / 2) - 1;
+
+    for (let i = middle; i < length-1; i++)
+    {
+        const firstHalfSum = array.slice(0,i).reduce( (a,b) => a+b);
+        const secondHalf = array.slice(i+1,length).reduce( (a,b) => a+b);
+
+        if (firstHalfSum === secondHalf)
+            return i;
+    }
+
+    return -1;
+}
